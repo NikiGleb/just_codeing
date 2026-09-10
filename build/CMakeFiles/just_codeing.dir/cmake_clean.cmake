8bc8af478294d04bd4f0bd4b719b3e298d3b748b
NikiGleb <nikihleb7@gmail.com>
2026-09-10 20:47:40 +0300

    q

---
+++ build/CMakeFiles/just_codeing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/just_codeing.dir/link.d"
+  "CMakeFiles/just_codeing.dir/main.cpp.o"
+  "CMakeFiles/just_codeing.dir/main.cpp.o.d"
+  "just_codeing"
+  "just_codeing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/just_codeing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
-
-int main(int, char**){
-    std::cout << "Hello, from just_codeing!\n";
+#include <cstdio>
+int maint(int argc, char** argv){
+    for(int i=0;i<argc;i++){
+        char* str = argv[i];
+        while(*str !=' \0'){
+            std::cout<<*str;
+            str++;
+        }
+    }
 }
+/*
+int main(int argc, char** argv){
+    FILE* f = fopen("data.txt","r");
+    
+    int max_cnt=-1;
+    char char_with_max_cnt;
+
+    int current_cnt=-1;
+    int current_char;
+    int prev_char=-1;
+
+    while((current_char = fgetc(f))!=EOF){
+        if(current_char!=prev_char){
+            if(current_cnt>max_cnt){
+                max_cnt=current_cnt;
+                char_with_max_cnt=char(prev_char);
+            }
+            prev_char=current_char;
+            current_cnt=1;
+        }
+        else{
+            current_cnt++;
+        }
+    }
+    if(current_cnt>max_cnt){
+        max_cnt=current_cnt;
+        char_with_max_cnt=char(prev_char);
+    }
+    if(max_cnt!=-1){
+        std::cout<<char_with_max_cnt<<" "<<max_cnt<<std::endl;
+    }
+    else{
+        std::cout<<"Empty file!"<<std::endl;
+    }
+}
+*/
